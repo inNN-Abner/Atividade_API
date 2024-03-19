678ec9416ab212183faacd6907bea19bf04386d5
inNN-Abner <abner.faculdade440@gmail.com>
2024-03-18 22:07:27 -0300

    Commit da Atividade_API

---
+++ src/routes/task/task.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import TaskController from '../../controllers/task/task.controller';
+
+const taskRoutes = Router();
+
+taskRoutes.post('/', TaskController.store);
+taskRoutes.get('/', TaskController.index);
+taskRoutes.get('/:id', TaskController.show);
+taskRoutes.delete('/:id', TaskController.delete);
+taskRoutes.put('/:id', TaskController.update);
+
+export default taskRoutes;
---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import dataBase from './database/ormconfig';
+import routes from './routes';
+import cors from 'cors';
+
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Servidor executando na porta ${port}`)
+  console.log(`Banco de dados`, dataBase.isInitialized ? 'inicializado' : 'não inicializado')
+});
